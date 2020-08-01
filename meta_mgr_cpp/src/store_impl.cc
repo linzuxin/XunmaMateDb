b90b6d7c68a06f72f40c0bd90244369849675906
林祖鑫 <linzuxin@linzuxindeMacBook.local>
2020-08-01 12:51:30 +0800

    test

--- meta_mgr_cpp/src/store_impl.cc
+++ meta_mgr_cpp/src/store_impl.cc
@@ -1,28 +1,39 @@
 #include "store_impl.h"
 #include "easylogging++.h"
-#include "replay.h"
 
-bool StoreImpl::Init(const char *dir) {
+bool StoreImpl::Init(const char *dir)
+{
   std::lock_guard<std::mutex> lock(mutex_);
   //Init data_mgr
   //Init meta_mgr
+  profile = initProfile();
+  memset(&hashList, 0, sizeof(HashTable) * HASH_LEN);
   return true;
 }
 
-void StoreImpl::Deinit() {
+void StoreImpl::Deinit()
+{
   std::lock_guard<std::mutex> lock(mutex_);
   //Deinit data_mgr
   //Deinit meta_mgr
 }
 
-bool StoreImpl::WriteDeltaPacket(const DeltaPacket &packet) {
+bool StoreImpl::WriteDeltaPacket(const DeltaPacket &packet)
+{
   std::lock_guard<std::mutex> lock(mutex_);
   // 1. save data to disk
   // 2. save version to meta manager
-  return true;
+  bool result = false;
+  setDataVersion(profile, packet.version);
+  for (uint16_t i = 0; i < packet.delta_count; ++i)
+  {
+    result = HashInsert(hashList, &packet.deltas[i], packet.version);
+  }
+  return result;
 }
 
-bool StoreImpl::ReadDataByVersion(uint64_t key, uint64_t version, Data &data) {
+bool StoreImpl::ReadDataByVersion(uint64_t key, uint64_t version, Data &data)
+{
   std::lock_guard<std::mutex> lock(mutex_);
   // 1. get deltas with versions are less or equal than 'version' from delta
   // manager
@@ -30,6 +41,10 @@ bool StoreImpl::ReadDataByVersion(uint64_t key, uint64_t version, Data &data) {
   // 2. read deltas from delta file
 
   // 3. apply all deltas
-  memset(&data, 0, sizeof(Data));
-  return true;
+  bool result = false;
+  if (validateDataVersion(profile, version))
+  {
+    result = HashSearch(hashList, key, version, data);
+  }
+  return result;
 }
